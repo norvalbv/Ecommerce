e5f2a533731a3ed4e965da6a20b07cdd7b970b18
norvalbv <benjinorval@gmail.com>
2021-12-27 19:38:53 +0000

    improved responsiveness for homepage

--- src/components/shopitems/shopitems.jsx
+++ src/components/shopitems/shopitems.jsx
@@ -1,10 +1,8 @@
 import React, {useState} from 'react';
-import img from '../../files/slide-1.jpg';
-import img2 from '../../files/slide-2.jpg';
-import img3 from '../../files/slide-3.jpg';
-import img4 from '../../files/landscape-slide-1.jpg';
-import img5 from '../../files/landscape-slide-2.jpg';
-import img6 from '../../files/landscape-slide-3.jpg';
+import img from '../../files/705tee.jpg';
+import img2 from '../../files/705tee2.jpg';
+import img3 from '../../files/705sweater.jpg';
+import img4 from '../../files/705sweater2.jpg';
 import './shopitems.scss';
 
 import { Link } from 'react-router-dom';
@@ -16,50 +14,53 @@ export default function ShopItems() {
 
     const [image, setImage] = useState(img);
     const [image2, setImage2] = useState(img3);
-    const [image3, setImage3] = useState(img5);
+    const [image3, setImage3] = useState(img);
+    const [image4, setImage4] = useState(img3);
 
     return (
-        <div className="shopitems">
-            <div className="itemContainer"
-                onMouseOver={() => setImage(img2)}
-                onMouseOut={() => setImage(img)}>
-                
-                <img src={image} alt="#" className="item-image"
-                />
-                <div className="overlay">
-                    <Link to='/' className='search-icon'><SearchIcon fontSize="large" className="icon"/></Link>
-                    <Link to='/categories' className='similar-btn'>View Similar</Link>
-                    <ShoppingBasketIcon fontSize="large" className="icon"/>
+        <div className="container">
+            <div className="shopitems">
+                <div className="itemContainer"
+                    onMouseOver={() => setImage(img2)}
+                    onMouseOut={() => setImage(img)}>
+                    
+                    <img src={image} alt="#" className="item-image"
+                    />
+                    <div className="overlay">
+                        <Link to='/' className='search-icon'><SearchIcon fontSize="large" className="icon"/></Link>
+                        <Link to='/categories' className='similar-btn'>View Similar</Link>
+                        <ShoppingBasketIcon fontSize="large" className="icon"/>
+                    </div>
                 </div>
-            </div>
-            <div className="itemContainer"
-                onMouseOver={() => setImage2(img4)}
-                onMouseOut={() => setImage2(img3)}>
-                <img src={image2} alt="#" className="item-image"/>
-                <div className="overlay">
-                    <Link to='/' className='search-icon'><SearchIcon fontSize="large" className="icon"/></Link>
-                    <Link to='/categories' className='similar-btn'>View Similar</Link>
-                    <ShoppingBasketIcon fontSize="large" className="icon"/>
+                <div className="itemContainer"
+                    onMouseOver={() => setImage2(img4)}
+                    onMouseOut={() => setImage2(img3)}>
+                    <img src={image2} alt="#" className="item-image"/>
+                    <div className="overlay">
+                        <Link to='/' className='search-icon'><SearchIcon fontSize="large" className="icon"/></Link>
+                        <Link to='/categories' className='similar-btn'>View Similar</Link>
+                        <ShoppingBasketIcon fontSize="large" className="icon"/>
+                    </div>
                 </div>
-            </div>
-            <div className="itemContainer"
-                onMouseOver={() => setImage3(img6)}
-                onMouseOut={() => setImage3(img5)}>
-                <img src={image3} alt="#" className="item-image"/>
-                <div className="overlay">
-                    <Link to='/' className='search-icon'><SearchIcon fontSize="large" className="icon"/></Link>
-                    <Link to='/categories' className='similar-btn'>View Similar</Link>
-                    <ShoppingBasketIcon fontSize="large" className="icon"/>
+                <div className="itemContainer"
+                    onMouseOver={() => setImage3(img2)}
+                    onMouseOut={() => setImage3(img)}>
+                    <img src={image3} alt="#" className="item-image"/>
+                    <div className="overlay">
+                        <Link to='/' className='search-icon'><SearchIcon fontSize="large" className="icon"/></Link>
+                        <Link to='/categories' className='similar-btn'>View Similar</Link>
+                        <ShoppingBasketIcon fontSize="large" className="icon"/>
+                    </div>
                 </div>
-            </div>
-            <div className="itemContainer"
-                onMouseOver={() => setImage(img2)}
-                onMouseOut={() => setImage(img)}>
-                <img src={image} alt="#" className="item-image"/>
-                <div className="overlay">
-                    <Link to='/' className='search-icon'><SearchIcon fontSize="large" className="icon"/></Link>
-                    <Link to='/categories' className='similar-btn'>View Similar</Link>
-                    <ShoppingBasketIcon fontSize="large" className="icon"/>
+                <div className="itemContainer"
+                    onMouseOver={() => setImage4(img4)}
+                    onMouseOut={() => setImage4(img3)}>
+                    <img src={image4} alt="#" className="item-image"/>
+                    <div className="overlay">
+                        <Link to='/' className='search-icon'><SearchIcon fontSize="large" className="icon"/></Link>
+                        <Link to='/categories' className='similar-btn'>View Similar</Link>
+                        <ShoppingBasketIcon fontSize="large" className="icon"/>
+                    </div>
                 </div>
             </div>
         </div>
